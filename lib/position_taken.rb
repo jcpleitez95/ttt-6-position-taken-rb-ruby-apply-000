88e851fa0edde9fbf15a75458f629bcb1148d3ca
essential-blog-3393 <jcpleitez@outlook.com>
2020-09-21 16:45:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,4 @@
 def position_taken?(board, index)
-  board[index] == " " || board[index] == ""
-  board[index] == nil
-  board[index] != " " || "" || nil
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+    return false
 end
